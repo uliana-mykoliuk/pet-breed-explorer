1bcb6833fa3d770003afb5fa1f1a6991c2ec3fc7
Uliana Mykoliuk <uliana.mykoliuk@gmail.com>
2024-08-12 11:23:35 +0300

    make responsive hero, add typescript for it

--- src/components/hero.component.tsx
+++ src/components/hero.component.tsx
@@ -1,30 +1,33 @@
 import Image from "next/image";
-
 import HeroDogBg from "@/assets/hero-dog-bg.jpg";
 
-const Hero = ({ onBtnClick }) => {
+interface HeroProps {
+  onBtnClick: () => void;
+}
+
+const Hero: React.FC<HeroProps> = ({ onBtnClick }) => {
   return (
-    <main className="h-screen bg-neutral-50 flex items-center px-[100px]">
-      <div className="flex items-center">
+    <main className="flex items-center min-h-screen bg-neutral-50 py-12 px-6 sm:px-12 md:px-24">
+      <div className="flex flex-col items-center md:flex-row">
         <Image
           src={HeroDogBg}
-          className="h-[450px] w-[450px] object-cover rounded-full"
-          alt=""
+          className="object-cover rounded-full h-36 w-36 md:h-80 md:w-80 xl:h-[450px] xl:w-[450px]"
+          alt="Hero background image"
         />
-        <div className="ml-[90px]">
-          <p className="text-[14px] text-green-800 font-bold tracking-[2px]">
-            Unlock your fluffy happyness
+        <div className="mt-5 text-center md:mt-0 md:ml-10 xl:ml-20 md:text-left">
+          <p className="font-bold tracking-widest text-xs text-green-800 md:text-sm">
+            Unlock your fluffy happiness
           </p>
-          <h1 className="text-yellow-500 text-[64px] leading-[70px]">
+          <h1 className="text-3xl text-yellow-500 md:text-5xl md:leading-tight">
             Pet Breed Explorer
           </h1>
-          <p className="text-[20px] mt-[16px] tracking-[1px]">
-            If you are not sure what your future friend looks like we are glad
-            to help you discover
+          <p className="mt-2 tracking-wide text-sm md:text-lg">
+            If you are not sure what your future friend looks like, we are glad
+            to help you discover.
           </p>
           <button
             onClick={onBtnClick}
-            className="bg-yellow-500 text-white py-[12px] px-[16px] rounded-full mt-[30px]"
+            className="mt-4 rounded-full bg-yellow-500 text-white text-sm py-1.5 px-4 md:mt-8 md:text-base md:py-3 md:px-6"
           >
             Let's have a look
           </button>
